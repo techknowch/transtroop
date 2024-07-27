fa591c2622e7bb7f556df5fe702126671c52a421
Ali Naqvi <techknowch@protonmail.com>
2024-07-27 20:18:29 +0500

    push assets

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,317 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title> Logistcwr - Transport and Logistics HTML5 Template </title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Favicon -->
+	<link rel="icon" sizes="56x56" href="assets/images/logo/fav-icon.png">
+	<!-- bootstrap CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- carousel CSS -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<!-- Animate Css -->
+	<link rel="stylesheet" href="assets/css/animate.css">			
+	<!-- Nice Select CSS -->
+	<link rel="stylesheet" href="assets/css/nice-select.css">		
+    <!-- Mobile Menu CSS -->
+	<link rel="stylesheet" href="assets/css/meanmenu.min.css">	
+	<!-- theme-default CSS -->
+	<link rel="stylesheet" href="assets/css/template-default.css">	
+    <!-- Owl Theme CSS -->
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">	
+	<!-- Main Style CSS -->
+	<link rel="stylesheet"  href="assets/css/style.css">
+	<!-- Commom Style CSS -->
+	<link rel="stylesheet"  href="assets/css/common.css">
+	<!-- venobox CSS -->
+	<link rel="stylesheet" href="venobox/venobox.css">
+    <!-- responsive CSS -->
+	<link rel="stylesheet" href="assets/css/responsive.css">	
+    <!-- bootstrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+	<!-- modernizr js -->	
+	<script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    
+</head>
+<body>
+
+<!-- pre loader area start -->
+<div id="loading">
+	<div id="loading-center">
+	<div id="loading-center-absolute">
+		<!-- loading content here -->
+		<div class="preloader__content text-center">
+			<div class="preloader__logo">
+				<img src="assets/images/logo/ts_logo.png" alt="">
+			</div>
+			<div id="st-loading-bar" class="preloader__bar">
+				<div id="st-loading-line" class="preloader__bar-inner"></div>
+			</div>
+		</div>
+	</div>
+	</div>  
+</div>
+<!-- pre loader area end -->
+
+<!-- Header Top Area -->
+<div class="st-header-top-area">
+	<div class="container container-large">
+		<div class="row">
+			<div class="col-lg-9">
+				<div class="st-header-top-info">
+					<ul>
+						<li><a href="#"><i class="bi bi-geo-alt-fill"></i> Location : 12W Profession Str Hobert, CA</a></li>
+						<li><a href="#"> <i class="bi bi-envelope"></i> Our Email : helpus24@gmail.com </a></li>
+						<li><a href="#"> <i class="bi bi-clock"></i> Office Time : Mon - Fri 8:00 - 6:30 </a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="st-header-top-social">
+					<span>Follow On:</span>
+					<ul>
+						<li><a href="#"> <i class="bi bi-facebook"></i> </a></li>
+						<li><a href="#"> <i class="bi bi-twitter"></i> </a></li>
+						<li><a href="#"> <i class="bi bi-linkedin"></i> </a></li>
+						<li><a href="#"> <i class="bi bi-instagram"></i> </a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Offcanvas area start -->
+<div class="st__offcanvas__area">
+	<div class="offcanvas__wrapper">
+		<div class="offcanvas__content">
+			<div class="offcanvas__top d-flex justify-content-between align-items-center">
+				<div class="offcanvas__logo">
+					<a href="index.html">
+						<img src="assets/images/logo/ts_logo.png" alt="logo not found">
+					</a>
+				</div>
+				<div class="offcanvas__close">
+					<button class="offcanvas-close-icon animation--flip">
+							<span class="offcanvas-m-lines">
+							<span class="offcanvas-m-line line--1"></span><span class="offcanvas-m-line line--2"></span><span class="offcanvas-m-line line--3"></span>
+							</span>
+					</button>
+				</div>
+			</div>
+			<div class="mobile-menu fix"></div>
+			<div class="offcanvas__social">
+				<h4 class="offcanvas__title mb-20">Subscribe & Follow</h4>
+				<ul class="header-top-socail-menu d-flex">
+					<li><a href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a></li>
+					<li><a href="https://twitter.com/"><i class="bi bi-twitter"></i></a></li>
+					<li><a href="https://www.pinterest.com/"><i class="bi bi-pinterest"></i></a></li>
+					<li><a href="https://vimeo.com/"><i class="bi bi-vimeo"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="offcanvas__overlay"></div>
+<!-- Header Area -->
+<header>
+    <div class="st-header-menu-area">
+		<div class="header-sticky">
+			<div class="st__logo__overlay"></div>
+			<div class="container container-large">
+				<div class="row align-items-center">
+					<div class="col-lg-2 col-md-6 col-sm-2">
+						<div class="st-header-logo">
+							<a href="index.html"><img src="assets/images/logo/ts_logo.png" alt="logo not found"></a>
+						</div>
+					</div>
+					<div class="col-lg-10 col-md-6 col-sm-10">
+						<div class="st__menu__wrap">
+							<div class="st__main__menu">
+								<nav class="main__menu" id="mobile-menu">
+									<ul>
+										<li><a href="#"> Home <i class="bi bi-chevron-down"></i> </a>
+											<ul class="sub__menu">
+												<li><a href="index.html">Home One</a></li>
+												<li><a href="index-two.html">Home Two</a></li>
+												<li><a href="index-dark.html">Home Dark</a></li>
+												<li><a href="index-two-dark.html">Home Dark Two</a></li>
+											</ul>
+										</li>
+										<li><a href="about-us.html"> About <i class="bi bi-chevron-down"></i> </a>
+											<ul class="sub__menu">
+												<li><a href="about-us.html">About Light</a></li>
+												<li><a href="about-us-dark.html">About Dark</a></li>
+											</ul>
+										</li>
+										<li><a href="service.html"> Services <i class="bi bi-chevron-down"></i> </a>
+											<ul class="sub__menu">
+												<li><a href="service.html">Service Light </a></li>
+												<li><a href="service-dark.html">Service Dark </a></li>
+												<li><a href="service-details.html">Service Details</a></li>
+											</ul>
+										</li>
+										<li><a href="#"> Pages <i class="bi bi-chevron-down"></i> </a>
+											<ul class="sub__menu">
+												<li><a href="team.html">Team</a></li>
+												<li><a href="team-dark.html">Team Dark</a></li>
+												<li><a href="team-details.html">Team Details</a></li>
+												<li><a href="project-grid.html">Project</a></li>
+												<li><a href="project-grid-dark.html">Project Dark</a></li>
+												<li><a href="project-details.html">Project Details</a></li>
+												<li><a href="faq.html"> FAQ </a></li>
+												<li><a href="faq-dark.html"> FAQ Dark</a></li>
+												<li><a href="404.html">404 Page</a></li>
+												<li><a href="404-dark.html">404 Page Dark</a></li>
+											</ul>
+										</li>
+										<li><a href="blog-standard.html"> News <i class="bi bi-chevron-down"></i> </a>
+											<ul class="sub__menu">
+												<li><a href="blog-standard.html">Blog Standard</a></li>
+												<li><a href="blog-standard-dark.html">Blog Standard Dark</a></li>
+												<li><a href="blog-details.html">Blog Details</a></li>
+											</ul>
+										</li>
+										<li><a href="contact-us.html"> Contacts <i class="bi bi-chevron-down"></i> </a>
+											<ul class="sub__menu">
+												<li><a href="contact-us.html">Contact</a></li>
+												<li><a href="contact-us-dark.html">Contact Dark</a></li>
+											</ul>
+										</li>
+									</ul>
+								</nav>
+							</div>
+							<div class="st__header__right">
+								<div class="st-contact-meta">
+									<div class="st__contact__meta__inner">
+										<div class="st-contact-icon">
+											<a href="tel:+562015550124"> <i class="bi bi-telephone"></i> </a>
+										</div>
+										<div class="st-contact-content">
+											<span>Emergency</span>
+											+56 (201) 555-0124
+										</div>
+									</div>
+								</div>
+								<div class="st__header__hamburger">
+									<div class="sidebar__toggle">
+										<a class="bar-icon" href="javascript:void(0)">
+											<span></span>
+											<span></span>
+											<span></span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+    </div>
+</header>
+
+@yield('content')
+
+
+<!-- ST Footer Area -->
+<div class="st-footer-area pt-120 pb-32" style="background-image: url(assets/images/footer/bg.jpg);background-position: center center;background-size: cover;background-repeat: no-repeat;">
+	<div class="container">
+		<div class="row ft_borer">
+			<div class="col-lg-3 col-md-6 wow fadeInUpBig">
+				<div class="st__footer__logo">
+					<a href="#"> <img src="assets/images/logo/ts_logo.png" alt=""> </a>
+				</div>
+				<div class="st__footer__text">
+					<p>Feugiat a ligula rutrum luctus primis ultrice nteger congue magna at pretium purus a pretium ligula rutrum and luctus</p>
+				</div>
+				<div class="st__social__icon">
+					<h2>Social Info</h2>
+					<a href="#"> <i class="bi bi-facebook"></i> </a>
+					<a href="#"> <i class="bi bi-twitter"></i> </a>
+					<a href="#"> <i class="bi bi-pinterest"></i> </a>
+					<a href="#"> <i class="bi bi-instagram"></i> </a>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6 wow fadeInUpBig">
+				<div class="st__footer__title">
+					<h2> Our Navigation </h2>
+				</div>
+				<div class="st__footer__link">
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> Airplane Fright </a>
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> About Portx </a>
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> Meet The Team </a>
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> News & Media </a>
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> Our Projects </a>
+				</div>
+			</div>			
+			<div class="col-lg-3 col-md-6 wow fadeInUpBig">
+				<div class="st__footer__title">
+					<h2> Our Service </h2>
+				</div>
+				<div class="st__footer__link">
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> Reliability & Punctuality </a>
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> Trusted Franchise </a>
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> Warehoues Storage </a>
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> Real Time Tracking </a>
+					<a href="#"> <i class="bi bi-chevron-double-right"></i> Transparent Pricing </a>
+				</div>
+			</div>			
+			<div class="col-lg-3 col-md-6 wow fadeInUpBig">
+				<div class="st__footer__title">
+					<h2> Newsletter </h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+				</div>
+				<div class="st__footer__subscribe__form p-relative mt-45">
+					<input type="email" name="email" placeholder="Email Address">
+					<button type="submit"> <i class="bi bi-chevron-double-right"></i> </button>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12 wow fadeInUpBig">
+				<div class="st__footer__copyright text-center">
+					<span> © Copyrights 2024 <a href="#"> sTheme-IT </a> All rights reserved. </span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Back to top-->
+<div class="prgoress_scrollup">
+	<svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+		<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+	</svg>
+</div>
+
+
+	<!-- jquery js -->	
+	<script src="assets/js/vendor/jquery-3.6.2.min.js"></script>
+	<!-- bootstrap js -->	
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!-- Nice Select -->
+	<script src="assets/js/nice-select.js"></script>
+	<!-- carousel js -->
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<!-- counterup js -->
+	<script src="assets/js/jquery.counterup.min.js"></script>
+	<!-- waypoints js -->
+	<script src="assets/js/waypoints.min.js"></script>
+	<!-- Wow Js -->
+	<script src="assets/js/wow.min.js"></script>
+    <!-- Mobile Menu Js -->
+	<script src="assets/js/jquery.meanmenu.js"></script>
+	<!-- venobox js -->
+	<script src="venobox/venobox.js"></script>
+	<!-- venobox min js -->
+	<script src="venobox/venobox.min.js"></script>
+	<!-- theme js -->	
+	<script src="assets/js/theme.js"></script>
+
+</body>
+
+</html>
